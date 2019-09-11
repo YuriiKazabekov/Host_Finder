1f2e4180d4d3ffc0309274f14de3867393f910af
Yurii Kazabekov <be4ka1291@gmail.com>
2019-09-11 17:50:18 +0300

    FG added

--- Saver+upload.py
+++ Saver+upload.py
@@ -1,37 +1,66 @@
 import pexpect
 import getpass
 from datetime import datetime
-#import sys
-
-# создание переменной в которой храниться дата создания конфига
-now = datetime.now()
-formatted_date = now.strftime('%Y-%m-%d')
-formatted_date
 
 
-user = 'admin'
-password = getpass.getpass()
+# переменная в которой храниться дата создания конфига
+formatted_date = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-devices_ip = ['10.0.1.250', '10.0.1.251', '10.0.1.252', '10.0.2.250', '10.0.2.251', '10.0.2.252',  '10.0.3.250', '10.0.3.251',  #'10.0.2.253',
+# переменная с IP сервера TFTP
+tftp_server = '10.0.4.127'
+
+#user = 'admin'
+#password = getpass.getpass()
+
+ACSW_user = 'admin'
+ACSW_password = getpass.getpass(prompt='ACSW_password:')
+
+FG_user = 'admin'
+FG_password = getpass.getpass(prompt='FG_password:')
+
+ACSW_ip = ['10.0.1.250', '10.0.1.251', '10.0.1.252', '10.0.2.250', '10.0.2.251', '10.0.2.252',  '10.0.3.250', '10.0.3.251',  #'10.0.2.253',
               '10.0.3.252', '10.0.3.253' , '10.0.4.250', '10.0.4.251', '10.0.4.252', '10.0.5.250', '10.0.5.251', '10.0.5.252', '10.0.6.250',
               '10.0.6.251', '10.0.6.253']
+              
+FG_ip = ['10.0.10.5']#, '10.10.20.254', '10.10.30.254', '10.10.20.254']              
+              
 
-for ip in devices_ip:
+
+for ip in ACSW_ip:
     print('Connection to device {}'.format(ip))
-    with pexpect.spawn('ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -c aes256-cbc {}@{}'.format(user, ip)) as ssh:        #-oKexAlgorithms=+diffie-hellman-group1-sha1 -c aes256-cbc ad it for connecting to old devices
+    with pexpect.spawn('ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -c aes256-cbc {}@{}'.format(ACSW_user, ip)) as ssh:        #-oKexAlgorithms=+diffie-hellman-group1-sha1 -c aes256-cbc ad it for connecting to old devices
 
         ssh.expect('[(yes/no?)>]')
         ssh.sendline('yes')
 
         ssh.expect('password:')
-        ssh.sendline(password)
+        ssh.sendline(ACSW_password)
 
         ssh.expect('[#>]')
         ssh.sendline('save')
        
         ssh.expect('[#>]')
-        ssh.sendline('upload cfg_toTFTP 10.0.4.127 dest_file {}_{}.cfg'.format(ip, formatted_date))
+        ssh.sendline('upload cfg_toTFTP {} dest_file /ACSW/{}_{}.cfg'.format(tftp_server, ip, formatted_date))
         
         ssh.expect('#')
+        print(ssh.before.decode('ascii'))
+        ssh.close()
+
+
+
+for ip in FG_ip:
+    print('Connection to device {}'.format(ip))
+    with pexpect.spawn('ssh {}@{}'.format(FG_user, ip)) as ssh:       
+
+        ssh.expect('[(yes/no?)>]')
+        ssh.sendline('yes')
+
+        ssh.expect('password:')
+        ssh.sendline(FG_password)
+       
+        ssh.expect('[#>]')
+        ssh.sendline('execute backup config tftp /Fortigate/{}_{}.conf {}'.format(ip, formatted_date, tftp_server))
+        
+        ssh.expect('[OK>]')
         print(ssh.before.decode('ascii'))
         ssh.close()
