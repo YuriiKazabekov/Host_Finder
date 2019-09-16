513c38aac4fe7db49266c017f950ca969f25cb9f
Yurii Kazabekov <be4ka1291@gmail.com>
2019-09-16 17:45:48 +0300

    init.py, differ, fdm added

---
+++ differv2.py
@@ -0,0 +1,19 @@
+import difflib
+
+
+
+d = difflib.Differ()
+
+file1 = "file_1.txt"
+file2 = "file_2.txt"
+text1_lines = open(file1).readlines()
+text2_lines = open(file2).readlines()
+
+diff = d.compare(text1_lines, text2_lines)
+'''
+for i in diff:
+    print(i)
+'''   
+#print('\n'.join(diff))
+
+print(list(d.compare(text1_lines, text2_lines))
